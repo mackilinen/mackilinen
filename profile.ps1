27098ef2164f7de5d9dfc3f0e0ae57094829509d
Markus Eriksson <markus.eriksson@nepa.com>
2021-04-09 13:33:19 +0200

    Update to oh-my-posh v3 and add terminal-icons

--- profile.ps1
+++ profile.ps1
@@ -1,25 +1,13 @@
-#Import required modules
-Import-Module 'posh-git'
-Import-Module 'oh-my-posh'
-if ($host.Name -eq 'ConsoleHost') {
-    Import-Module PSReadLine
-}
-
 #Set Default user for oh-my-posh to avoid username in prompt
-$DefaultUser = 'markus.eriksson'
+$global:DefaultUser = [System.Environment]::UserName
 
-# Default the prompt to agnoster oh-my-posh theme
-Set-Theme paradox
-# Set location
-# set-location "C:\Repos"
+#Import required modules
+Import-Module PSReadLine
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module Terminal-Icons
 
-# Ensure that Get-ChildItemColor is loaded
-Import-Module Get-ChildItemColor
-
-# Set l and ls alias to use the new Get-ChildItemColor cmdlets
-Set-Alias l Get-ChildItemColor -Option AllScope
-Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
-Set-Alias dir Get-ChildItemColorFormatWide -Option AllScope
+Set-PoshPrompt -Theme paradox
 
 #utility functions
 
