02db50487dd7b232556727f89ad8e99d157a00d6
Markus Eriksson <markus.eriksson@nepa.com>
2022-06-17 07:34:29 -0500

    Add oh-my-posh theme

--- powershell_profile.ps1
+++ powershell_profile.ps1
@@ -3,7 +3,7 @@ Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
 
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\probua.minimal.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config ~/.mytheme.omp.json | Invoke-Expression
 
 #utility functions
 
@@ -11,3 +11,5 @@ function repos { set-location "C:\repos" }
 function .. { set-location ".." }
 function ... { set-location "..\.." }
 function .... { set-location "..\..\.." }
+function kubectl-pxy([Parameter(ValueFromRemainingArguments = $true)]$params) { $env:HTTPS_PROXY='127.0.0.1:55555'; & kubectl $params }
+function gcp-tunnel { cmd /c 'gcloud compute ssh bastion01 --tunnel-through-iap --project nepa-common-vault --zone europe-north1-b -- -L 55555:127.0.0.1:60000 -N' & }
