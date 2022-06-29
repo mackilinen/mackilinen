01308f9c5e2ae6f7d17aa7d025c0454d1fbc9885
Markus Eriksson <markus.eriksson@nepa.com>
2022-06-29 15:13:24 +0200

    Add winget packages

--- winget_install.ps1
+++ winget_install.ps1
@@ -1,6 +1,7 @@
 winget install -e --id 7zip.7zip
 winget install -e --id Amazon.Games
 winget install -e --id AnyDeskSoftwareGmbH.AnyDesk
+# winget install -e --id CoreyButler.NVMforWindows - I haven't needed it so far
 winget install -e --id Discord.Discord
 winget install -e --id Docker.DockerDesktop
 winget install -e --id ElectronicArts.EADesktop
@@ -15,12 +16,17 @@ winget install -e --id LogMeIn.LastPass
 winget install -e --id Microsoft.AzureDataStudio
 winget install -e --id Microsoft.dotnet
 winget install -e --id Microsoft.dotNetFramework
+winget install -e --id Microsoft.dotnetUninstallTool
 winget install -e --id Microsoft.PowerShell
 winget install -e --id Microsoft.PowerToys
+winget install -e --id Microsoft.RemoteDesktopClient
+winget install -e --id Microsoft.SQLServerManagementStudio
 winget install -e --id Microsoft.Teams
 winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Microsoft.WindowsPCHealthCheck
 winget install -e --id Microsoft.WindowsTerminal
 winget install -e --id Mozilla.Firefox.DeveloperEdition
+winget install -e --id NickeManarin.ScreenToGif
 winget install -e --id Nvidia.GeForceExperience
 winget install -e --id OpenJS.NodeJS
 winget install -e --id Postman.Postman
